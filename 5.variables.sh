39a394c817376cfc3d7b1d20e95e88949af7a2a5
Manjunatha <manjunathacloud1657@gmail.com>
2025-09-20 11:32:07 +0530

    changed

--- 5.variables.sh
+++ 5.variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-echo "Please enter you PIN
+echo "Please enter you PIN"
 read -s PIN_NUMBER # here enter your PIN number
-echo "PIN entered is #PIN_NUMBER"
+echo "PIN entered is: #PIN_NUMBER"
