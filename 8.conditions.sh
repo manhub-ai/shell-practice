3cb696e6d73fee81297e2f4476f4b30a34b73121
Manjunatha <manjunathacloud1657@gmail.com>
2025-09-23 15:34:45 +0530

    New Files

--- 8.conditions.sh
+++ 8.conditions.sh
@@ -3,7 +3,7 @@
 echo "please enter a number:"
 read Number
 
-if [ ((#NUMBER % 2)) -eq 0 ]; then
+if [ $((#NUMBER % 2)) -eq 0 ]; then
     echo "the entered Number $NUMBER is even"
 else
     echo "the entered number $NUMBER is odd"
