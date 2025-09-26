f08e8927759bda23e7717d1b77f469a1f122d9e6
Manjunatha <manjunathacloud1657@gmail.com>
2025-09-27 04:40:04 +0530

    conversation

---
+++ 04.variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "#PERSON1: Hey #PERSON2, what's up?"
+echo "#PERSON2: Not much, just relaxing. You?"
+echo "#PERSON1: Same here. Thought I'd check in."
+echo "#PERSON2: Glad you did. Been a while!"
+echo "#PERSON1: Yeah, we should hang out soon."
+echo "#PERSON2: Totally. Let's plan something this week."
+echo "#PERSON1: Cool, I'll text you later."
+echo "#PERSON2: Sounds good. Talk soon!"
