adf875494d3c16158f58b7923d413ba4bce16ce1
Manjunatha <manjunathacloud1657@gmail.com>
2025-09-27 06:14:33 +0530

    conversation

--- 09.data-types.sh
+++ 09.data-types.sh
@@ -7,3 +7,13 @@ NAME="Manju"
 SUM=$(($NUMBER1+$NUMBER2+$NAME))
 
 echo "Sum is $SUM"
+
+CRICKET_TEAM=("Abshick" "Dhoni" "Kohli" "Rohit" "Jadeja")
+
+
+# Size=4, Max index=5
+echo "Players in the team are: ${CRICKET_TEAM[@]}"
+echo "Captain of the team is: ${CRICKET_TEAM[2]}"
+echo "Wicket keeper of the team is: ${CRICKET_TEAM[1]}"
+echo "Bowler of the team is: ${CRICKET_TEAM[4]}"
+echo "First slip fielder of the team is: ${CRICKET_TEAM[0]}"
