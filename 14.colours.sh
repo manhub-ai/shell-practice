d5ca3b5d435f7960c48831094ef5d846bac5f39c
Manjunatha <manjunathacloud1657@gmail.com>
2025-09-27 10:40:45 +0530

    conversation

---
+++ 14.colours.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Colour Variables
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"      
+B="\e[34m"
+M="\e[35m"
+C="\e[36m"
+W="\e[37m"
+N="\e[0m"
+
+echo -e "$M Hello Manjunatha, $C Good Morning $N"
+echo -e " $R check this colour RED $N"
