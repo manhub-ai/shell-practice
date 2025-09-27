a3ca3e2d696b38b4157952e6313d8350ddeb2adb
Manjunatha <manjunathacloud1657@gmail.com>
2025-09-27 10:05:14 +0530

    conversation

--- 10.conditions.sh
+++ 10.conditions.sh
@@ -3,9 +3,9 @@
 # This script checks if the given number is greater than, less than or equal to 10
 NUMBER=$1
 
-if [ $NUMBER -ne 0 ] ; then
+if [ $NUMBER -lt 0 ] ; then
     echo "The given number $NUMBER is grater than 10"
-elif [ $NUMBER -ne 10 ] ; then
+elif [ $NUMBER -eq 10 ] ; then
     echo "The given number $NUMBER is equal to 10"
 else
     echo "The given number $NUMBER is less than 10"
